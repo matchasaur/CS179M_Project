a4ca44fe2e91d2d51c9e05bbbb9daa044ff94343
AaronH2021 <86315573+AaronH2021@users.noreply.github.com>
2023-03-09 17:31:55 -0800

    Grid and Test Log
    Grid: When user wants to select which containers to onload
    Test Log: Found a test log, need help figuring out how to actually log certain actions

---
+++ GUI/grid.py
@@ -0,0 +1,305 @@
+import tkinter
+
+master=tkinter.Tk()
+master.title("grid() method")
+master.geometry("1000x1000")
+
+L1=tkinter.Label( text ="Select which containers need to be offloaded:", font = 40)
+L1.grid(row=0, column=0)
+
+button10=tkinter.Button(master, text="Amazon")
+button10.grid(row=1,column=0)
+
+button11=tkinter.Button(master, text="Amazon")
+button11.grid(row=1,column=1)
+
+button12=tkinter.Button(master, text="Amazon")
+button12.grid(row=1,column=2)
+
+button13=tkinter.Button(master, text="Amazon")
+button13.grid(row=1,column=3)
+
+button14=tkinter.Button(master, text="Amazon")
+button14.grid(row=1,column=4)
+
+button15=tkinter.Button(master, text="Amazon")
+button15.grid(row=1,column=5)
+
+button16=tkinter.Button(master, text="Amazon")
+button16.grid(row=1,column=6)
+
+button17=tkinter.Button(master, text="Amazon")
+button17.grid(row=1,column=7)
+
+button18=tkinter.Button(master, text="Amazon")
+button18.grid(row=1,column=8)
+
+button19=tkinter.Button(master, text="Amazon")
+button19.grid(row=1,column=9)
+
+button110=tkinter.Button(master, text="Amazon")
+button110.grid(row=1,column=10)
+
+button111=tkinter.Button(master, text="Amazon")
+button111.grid(row=1,column=11)
+
+button20=tkinter.Button(master, text="Amazon")
+button20.grid(row=2,column=0)
+
+button21=tkinter.Button(master, text="Amazon")
+button21.grid(row=2,column=1)
+
+button22=tkinter.Button(master, text="Amazon")
+button22.grid(row=2,column=2)
+
+button23=tkinter.Button(master, text="Amazon")
+button23.grid(row=2,column=3)
+
+button24=tkinter.Button(master, text="Amazon")
+button24.grid(row=2,column=4)
+
+button25=tkinter.Button(master, text="Amazon")
+button25.grid(row=2,column=5)
+
+button26=tkinter.Button(master, text="Amazon")
+button26.grid(row=2,column=6)
+
+button27=tkinter.Button(master, text="Amazon")
+button27.grid(row=2,column=7)
+
+button28=tkinter.Button(master, text="Amazon")
+button28.grid(row=2,column=8)
+
+button29=tkinter.Button(master, text="Amazon")
+button29.grid(row=2,column=9)
+
+button210=tkinter.Button(master, text="Amazon")
+button210.grid(row=2,column=10)
+
+button211=tkinter.Button(master, text="Amazon")
+button211.grid(row=2,column=11)
+
+button30=tkinter.Button(master, text="Amazon")
+button30.grid(row=3,column=0)
+
+button31=tkinter.Button(master, text="Amazon")
+button31.grid(row=3,column=1)
+
+button32=tkinter.Button(master, text="Amazon")
+button32.grid(row=3,column=2)
+
+button33=tkinter.Button(master, text="Amazon")
+button33.grid(row=3,column=3)
+
+button34=tkinter.Button(master, text="Amazon")
+button34.grid(row=3,column=4)
+
+button35=tkinter.Button(master, text="Amazon")
+button35.grid(row=3,column=5)
+
+button36=tkinter.Button(master, text="Amazon")
+button36.grid(row=3,column=6)
+
+button37=tkinter.Button(master, text="Amazon")
+button37.grid(row=3,column=7)
+
+button38=tkinter.Button(master, text="Amazon")
+button38.grid(row=3,column=8)
+
+button39=tkinter.Button(master, text="Amazon")
+button39.grid(row=3,column=9)
+
+button310=tkinter.Button(master, text="Amazon")
+button310.grid(row=3,column=10)
+
+button311=tkinter.Button(master, text="Amazon")
+button311.grid(row=3,column=11)
+
+button40=tkinter.Button(master, text="Amazon")
+button40.grid(row=4,column=0)
+
+button41=tkinter.Button(master, text="Amazon")
+button41.grid(row=4,column=1)
+
+button42=tkinter.Button(master, text="Amazon")
+button42.grid(row=4,column=2)
+
+button43=tkinter.Button(master, text="Amazon")
+button43.grid(row=4,column=3)
+
+button44=tkinter.Button(master, text="Amazon")
+button44.grid(row=4,column=4)
+
+button45=tkinter.Button(master, text="Amazon")
+button45.grid(row=4,column=5)
+
+button46=tkinter.Button(master, text="Amazon")
+button46.grid(row=4,column=6)
+
+button47=tkinter.Button(master, text="Amazon")
+button47.grid(row=4,column=7)
+
+button48=tkinter.Button(master, text="Amazon")
+button48.grid(row=4,column=8)
+
+button49=tkinter.Button(master, text="Amazon")
+button49.grid(row=4,column=9)
+
+button410=tkinter.Button(master, text="Amazon")
+button410.grid(row=4,column=10)
+
+button411=tkinter.Button(master, text="Amazon")
+button411.grid(row=4,column=11)
+
+button50=tkinter.Button(master, text="Amazon")
+button50.grid(row=5,column=0)
+
+button51=tkinter.Button(master, text="Amazon")
+button51.grid(row=5,column=1)
+
+button52=tkinter.Button(master, text="Amazon")
+button52.grid(row=5,column=2)
+
+button53=tkinter.Button(master, text="Amazon")
+button53.grid(row=5,column=3)
+
+button54=tkinter.Button(master, text="Amazon")
+button54.grid(row=5,column=4)
+
+button55=tkinter.Button(master, text="Amazon")
+button55.grid(row=5,column=5)
+
+button56=tkinter.Button(master, text="Amazon")
+button56.grid(row=5,column=6)
+
+button57=tkinter.Button(master, text="Amazon")
+button57.grid(row=5,column=7)
+
+button58=tkinter.Button(master, text="Amazon")
+button58.grid(row=5,column=8)
+
+button59=tkinter.Button(master, text="Amazon")
+button59.grid(row=5,column=9)
+
+button510=tkinter.Button(master, text="Amazon")
+button510.grid(row=5,column=10)
+
+button511=tkinter.Button(master, text="Amazon")
+button511.grid(row=5,column=11)
+
+button60=tkinter.Button(master, text="Amazon")
+button60.grid(row=6,column=0)
+
+button61=tkinter.Button(master, text="Amazon")
+button61.grid(row=6,column=1)
+
+button62=tkinter.Button(master, text="Amazon")
+button62.grid(row=6,column=2)
+
+button63=tkinter.Button(master, text="Amazon")
+button63.grid(row=6,column=3)
+
+button64=tkinter.Button(master, text="Amazon")
+button64.grid(row=6,column=4)
+
+button65=tkinter.Button(master, text="Amazon")
+button65.grid(row=6,column=5)
+
+button66=tkinter.Button(master, text="Amazon")
+button66.grid(row=6,column=6)
+
+button67=tkinter.Button(master, text="Amazon")
+button67.grid(row=6,column=7)
+
+button68=tkinter.Button(master, text="Amazon")
+button68.grid(row=6,column=8)
+
+button69=tkinter.Button(master, text="Amazon")
+button69.grid(row=6,column=9)
+
+button610=tkinter.Button(master, text="Amazon")
+button610.grid(row=6,column=10)
+
+button611=tkinter.Button(master, text="Amazon")
+button611.grid(row=6,column=11)
+
+button70=tkinter.Button(master, text="Amazon")
+button70.grid(row=7,column=0)
+
+button71=tkinter.Button(master, text="Amazon")
+button71.grid(row=7,column=1)
+
+button72=tkinter.Button(master, text="Amazon")
+button72.grid(row=7,column=2)
+
+button73=tkinter.Button(master, text="Amazon")
+button73.grid(row=7,column=3)
+
+button74=tkinter.Button(master, text="Amazon")
+button74.grid(row=7,column=4)
+
+button75=tkinter.Button(master, text="Amazon")
+button75.grid(row=7,column=5)
+
+button76=tkinter.Button(master, text="Amazon")
+button76.grid(row=7,column=6)
+
+button77=tkinter.Button(master, text="Amazon")
+button77.grid(row=7,column=7)
+
+button78=tkinter.Button(master, text="Amazon")
+button78.grid(row=7,column=8)
+
+button79=tkinter.Button(master, text="Amazon")
+button79.grid(row=7,column=9)
+
+button710=tkinter.Button(master, text="Amazon")
+button710.grid(row=7,column=10)
+
+button711=tkinter.Button(master, text="Amazon")
+button711.grid(row=7,column=11)
+
+button80=tkinter.Button(master, text="Amazon")
+button80.grid(row=8,column=0)
+
+button81=tkinter.Button(master, text="Amazon")
+button81.grid(row=8,column=1)
+
+button82=tkinter.Button(master, text="Amazon")
+button82.grid(row=8,column=2)
+
+button83=tkinter.Button(master, text="Amazon")
+button83.grid(row=8,column=3)
+
+button84=tkinter.Button(master, text="Amazon")
+button84.grid(row=8,column=4)
+
+button85=tkinter.Button(master, text="Amazon")
+button85.grid(row=8,column=5)
+
+button86=tkinter.Button(master, text="Amazon")
+button86.grid(row=8,column=6)
+
+button87=tkinter.Button(master, text="Amazon")
+button87.grid(row=8,column=7)
+
+button88=tkinter.Button(master, text="Amazon")
+button88.grid(row=8,column=8)
+
+button89=tkinter.Button(master, text="Amazon")
+button89.grid(row=8,column=9)
+
+button810=tkinter.Button(master, text="Amazon")
+button810.grid(row=8,column=10)
+
+button811=tkinter.Button(master, text="Amazon")
+button811.grid(row=8,column=11)
+
+def nextPage():
+    master.destroy()
+    import manifest
+    
+B1=tkinter.Button(master, text="Next", font=40, command=nextPage)
+B1.grid(row=9, column=0)
+
+master.mainloop()
