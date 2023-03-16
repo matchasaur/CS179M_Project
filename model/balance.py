22dd69c1b30e69cdad9f7038a4240b8d00a9c692
Jared.Tan <Jtanu45@gmail.com>
2023-03-16 00:05:30 -0700

    refactored the cargo representation to use a 2-D lists instead of 1-D list

--- model/balance.py
+++ model/balance.py
@@ -1,5 +1,19 @@
 from queue import PriorityQueue
+import heapq
 
+#PriorityQueue implementation
+class PriorityQueue:
+    def __init__(self):
+        self.nodes = []
+        
+    def insert(self, Node, priority):
+        heapq.heappush(self.nodes, (priority, Node))
+        
+    def pop(self):
+        return heapq.heappop(self.elements)[1]
+    
+    def isEmpty(self):
+        return len(self.nodes) == 0
 
 #Class declaration/definitions
 class Node:
@@ -7,17 +21,25 @@ class Node:
     def __init__(self, shipcargo):
         self.currCargo = shipcargo
         self.balanceScore = self.calculateBalance(shipcargo)
+        self.cost = 0
     
     def calculateBalance(self, currCargo):
         leftWeight = 0
         rightWeight = 0
         
         for i in currCargo:
-            if (i.x < 3):
-                leftWeight += i.weight
-            else:
-                rightWeight += i.weight
+            for j in i:
+                if (j.x < 3):
+                    leftWeight += j.weight
+                else:
+                    rightWeight += j.weight
         return min(leftWeight,rightWeight)/max(leftWeight,rightWeight)
+    
+    def columnNotEmpty(self, column):
+        for i in self.currCargo:
+            if (i.x == column & i.name != 'UNUSED'):
+                return True
+        return False
 
 class Container:
     def __init__(self, y, x, weight, name):
@@ -30,27 +52,56 @@ class Container:
 
 
 #Meat and Potatoes
-def search(Graph, ):
-    pass
+# def search(start):
+#     #All visited nodes are placed into a set
+#     setOfVisitedNodes = {}
+    
+#     queue = PriorityQueue()
+#     queue.insert(start, start.balanceScore)
+#     parent = {}
+#     cost_so_far = {}
+#     parent[start] = None
+#     cost_so_far[start] = 0
+    
+#     while not queue.isEmpty():
+#         #Grab the node at the top of the queue
+#         currNode = queue.pop()
+        
+#         #If the node is admissible, we can sail
+#         if isAdmissible(currNode):
+#             return currNode
+
+#         #Otherwise, we need to expand the nodes
+#         expandedNodes = []
+        
 
 
+# def expandNode(expandNode, expandedNodes):
+#     #We cycle the crane over each column, left to right
+#     for i in 6:
+#         #Find the columns that are not empty
+#         if(expandNode.isNotEmpty(i)):
+#             #Move container
+#             tempContainer 
+
+# def isAdmissible(node):
+#     if (node.balanceScore >= 0.9):
+#         return True 
 
 #Menial functions...
 def displayCargo(shipcargo):
     for i in shipcargo:
-        print(i.name.ljust(5), ' ',end='')
-        if (i.x == 5):
-            print('\n')
-        
-def calculateWeight(shipcargo, side):
-     pass   
+        for j in i:
+            print(j.name.center(12), ' ', end='')
+        print('')
 
 if __name__ == "__main__":
     #First things first, we need to populate a list of lists with the information of the manifest
     
     file = open('model/balanceTest1.txt', 'r')
     
-    shipcargo = []
+    shipcargo = [[0] * 6 for i in range(2)]
+    
     
     for line in file:
         parts = line.strip().split(", ")
@@ -61,13 +112,13 @@ if __name__ == "__main__":
         weight = parts[1].strip("{}")
         name = parts[2]
         
-        y = int(y)
-        x = int(x)
+        y = int(y) - 1
+        x = int(x) - 1
         weight = int(weight)
         # print(y,x)
         
         tempContainer = Container(y,x,weight,name)
-        shipcargo.append(tempContainer)
+        shipcargo[y][x] = tempContainer
         
     displayCargo(shipcargo)
     
