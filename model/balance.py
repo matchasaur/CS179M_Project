4606a7b822951e1a3da0bb829334e1143a35710e
Jared.Tan <Jtanu45@gmail.com>
2023-03-23 01:12:32 -0700

    Added SIFT functionality

--- model/balance.py
+++ model/balance.py
@@ -16,6 +16,7 @@ class PriorityQueue:
         heapq.heappush(self.nodes, (priority, currNode))    
     
     def pop(self):
+        heapq.heapify(self.nodes)
         return heapq.heappop(self.nodes)[1]
     
     def isEmpty(self):
@@ -75,8 +76,8 @@ class Node:
     
 class Container:
     def __init__(self, y, x, weight, name):
-        self.y = y-1
-        self.x = x-1
+        self.y = y
+        self.x = x
         self.weight = weight
         self.name = name
         keyPair = (y,x)
@@ -109,7 +110,7 @@ class cargoHash():
         return False
 
 #Meat and Potatoes
-def search(start):
+def search(start, listByWeight):
     #All visited nodes are placed into a set
     setOfVisitedNodes = cargoHash()
     setOfVisitedNodes.insertItem(start.cargo)
@@ -131,8 +132,9 @@ def search(start):
         # print()
         #If the node is admissible, we can sail
         if isAdmissible(currNode):
-            print("Yo we found the goal!!!!!!!")
-            print("balanceScore: ", currNode.balanceScore, ', ', 'cost: ', currNode.cost)
+            # print("Yo we found the goal!!!!!!!")
+            print("Found admissible configuration:")
+            print("balanceScore: ", currNode.balanceScore, ', ', 'cost: ', currNode.cost*4)
             displayCargo(currNode.cargo)
             return currNode
 
@@ -140,9 +142,90 @@ def search(start):
         expandNode(currNode, queue, setOfVisitedNodes, parent)
 
     print("Yo we didn't find the goal...")
+    SIFT(start, listByWeight)
+    
+
+def SIFT(start, listByWeight):
+    print("Starting SIFT function:")
+    #First construct goal state
+    copyStart = copy.deepcopy(start)
+    goalState = constructGoal(start, listByWeight)
+    copyStart.cost = updateCost(copyStart, listByWeight)
+    # print("cost: ", copyStart.cost)
+    #Then run manhattan distance on goal
+    setOfVisitedNodes = cargoHash()
+    setOfVisitedNodes.insertItem(copyStart.cargo)
+    queue = PriorityQueue()
+    queue.insert(copyStart, (1-copyStart.balanceScore) + copyStart.cost)
+    # queue.insert(start)
+    parent = {}
+    parent[start] = None
+
+    
+    while not queue.isEmpty():
+        #Grab the node at the top of the queue
+        currNode = queue.pop()
+        # ///////////////////// DEBUG MESSAGE /////////////////////////
+        # print("func::search(): Observing this cargo: ")
+        # displayCargo(currNode.cargo)
+        # print("Cost: ", currNode.cost)
+        # currNode.updateBalance()
+        # print("Balance Score: ", currNode.balanceScore)
+        # print()
+        #If the node is admissible, we can sail
+        if (currNode.cost == 0):
+            print("Constructed SIFT admissible configuration:")
+            currNode.updateBalance()
+            print("balanceScore: ", currNode.balanceScore, ', ', 'cost: ', copyStart.cost*4)
+            displayCargo(currNode.cargo)
+            return currNode
+
+        #Otherwise, we need to expand the nodes
+        expandSIFTNode(currNode, queue, setOfVisitedNodes, parent, listByWeight)
+    
+
+def constructGoal(start, listByWeight):
+    goalCargo = copy.deepcopy(start)
+    for i in range(8):
+        for j in range(12):
+            if(goalCargo.cargo[i][j].name != 'NAN'):
+                goalCargo.cargo[i][j].clear()
+    
+    #Populate
+    leftIndex = 5
+    rightIndex = 6
+    rowIndex = 0
+    for j in range(len(listByWeight)):
+        #print("Inserting ", listByWeight[j].name)
+        if(leftIndex == 12 and rightIndex == -1):
+            rowIndex+=1
+            leftIndex = 5
+            rightIndex = 6
+        if (j%2 == 0):
+            if (goalCargo.cargo[rowIndex][rightIndex].name == 'NAN'):
+                break
+            # print("Inserting Right...")
+            listByWeight[j].y = rowIndex
+            listByWeight[j].x = rightIndex
+            #print(listByWeight[j].name,':',listByWeight[j].y,listByWeight[j].x)
+            goalCargo.cargo[rowIndex][rightIndex] = listByWeight[j]
+            rightIndex += 1
+        else:
+            if (goalCargo.cargo[rowIndex][leftIndex].name == 'NAN'):
+                break
+            #print("Inserting Left...")
+            listByWeight[j].y = rowIndex
+            listByWeight[j].x = leftIndex
+            #print(listByWeight[j].name,':',listByWeight[j].y,listByWeight[j].x)
+            goalCargo.cargo[rowIndex][leftIndex] = listByWeight[j]
+            leftIndex -= 1
+
+    displayCargo(goalCargo.cargo)
+    return goalCargo
     
 def manhatDist(x1, x2, y1, y2):
-    return (abs(x2-x1) + abs(y2-y1))*4
+    # print(x1, x2, y1, y2)
+    return (abs(x2-x1) + abs(y2-y1))
 
 
 def expandNode(nodeToExpand, queue, setOfVisitedNodes, parent):
@@ -184,6 +267,60 @@ def expandHelper(node, coordinates, queue, setOfVisitedNodes, parent):
             #////////////////////////////DEBUG MESSAGE////////////////////////////////
             #print("func::expandHelper(): Skipping cell...")
             
+def expandSIFTNode(nodeToExpand, queue, setOfVisitedNodes, parent, listByWeight):
+    #We cycle the crane over each column, left to right
+    for i in range(columns):
+        #Find the columns that are not empty
+        if((-1,-1) != nodeToExpand.columnNotEmpty(i)):
+            #Expand this container by moving it to a valid position
+            coordinates = nodeToExpand.columnNotEmpty(i)
+            #////////////////////////////DEBUG MESSAGE////////////////////////////////
+            # print("func::expandNode(): ", coordinates)
+            # print("func::expandNode(): Shifting container: ", nodeToExpand.cargo[coordinates[0]][coordinates[1]].name)
+            expandSIFTHelper(nodeToExpand, coordinates, queue, setOfVisitedNodes, parent, listByWeight)
+            
+def expandSIFTHelper(node, coordinates, queue, setOfVisitedNodes, parent, listByWeight):
+    #For each column, find a valid cell to move the container to
+    for i in range(columns):
+        y, x = node.nearestContainer(i)
+        #////////////////////////////DEBUG MESSAGE////////////////////////////////
+        # print("func::expandHelper(): We are looking at location:", y, ' ', x)
+        if (i != coordinates[1] and y != rows):
+            tempNode = copy.deepcopy(node)
+            tempNode.cargo[y][i].weight = node.cargo[coordinates[0]][coordinates[1]].weight
+            tempNode.cargo[y][i].name = node.cargo[coordinates[0]][coordinates[1]].name
+            tempNode.cargo[coordinates[0]][coordinates[1]].clear()
+            #////////////////////////////DEBUG MESSAGE////////////////////////////////
+            # displayCargo(tempNode.cargo); print()
+            if not setOfVisitedNodes.isRepeated(tempNode.cargo):
+                newCost = updateCost(tempNode, listByWeight)
+                tempNode.updateCost(newCost)
+                #////////////////////////////DEBUG MESSAGE////////////////////////////////
+                # print("Adding this state to the queue:")
+                # displayCargo(tempNode.cargo)
+                # print("Cost: ", newCost)
+                priority = newCost
+                tempNode.balanceScore = 0
+                setOfVisitedNodes.insertItem(tempNode.cargo)
+                parent[tempNode] = node
+                queue.insert(tempNode, priority)
+
+                
+def updateCost(node, listByWeight):
+    # print("\tUpdating cost...")
+    total = 0
+    # displayCargo(node.cargo)
+    for i in listByWeight:
+        # print("Searching for: ", i.name)
+        for j in range(12):
+            for k in reversed(range(8)):
+                if (node.cargo[k][j].name == 'NAN'):
+                    break
+                if (i.name == node.cargo[k][j].name):
+                    # print("Found ", i.name)
+                    total += manhatDist(i.x, node.cargo[k][j].x, i.y, node.cargo[k][j].y)
+                    # print("Total: ", total)
+    return total
 
 def isAdmissible(node):
     if (node.balanceScore > 0.9):
@@ -196,11 +333,20 @@ def displayCargo(shipcargo):
             print(j.name.center(12), ' ', end='')
         print('')
         
+def displayCoord(shipcargo):
+    for i in reversed(shipcargo):
+        for j in (i):
+            print('(', j.y, ',', j.x, ')', end='')
+        print('')
+        
 def displayWeight(shipcargo):
     for i in reversed(shipcargo):
         for j in reversed(i):
             print(str(j.weight).center(12), ' ', end='')
         print('')
+
+def weightFunc(container):
+    return container.weight
 
 if __name__ == "__main__":
     #First things first, we need to populate a list of lists with the information of the manifest
@@ -210,10 +356,10 @@ if __name__ == "__main__":
     
     
     
-    file = open('ShipCase3.txt', 'r')
+    file = open('ShipCase5.txt', 'r')
     
     shipcargo = [[0] * columns for i in range(rows)]
-    
+    listByWeight = []
     
     for line in file:
         parts = line.strip().split(", ")
@@ -231,17 +377,20 @@ if __name__ == "__main__":
         
         tempContainer = Container(y,x,weight,name)
         shipcargo[y][x] = tempContainer
+        if(tempContainer.name != 'UNUSED' and tempContainer.name != 'NAN'):
+            listByWeight.append(tempContainer)
         
+    listByWeight.sort(key=weightFunc, reverse=True)
     displayCargo(shipcargo)
     
     testNode = Node(shipcargo)
     
     print("Attempting to balance...")
     starttime = timeit.default_timer()
-    search(testNode)
+    search(testNode, listByWeight)
     stoptime = timeit.default_timer()
     execution_time = stoptime - starttime
-    print("Program ran in ", str(execution_time), " seconds.")
+    print("Program ran in ", "{0:.4f}".format(execution_time), " seconds.")
     # print(testNode.balanceScore)
     # print(testNode.columnNotEmpty(5))
     
